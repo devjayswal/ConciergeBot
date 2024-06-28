9fc2f7d58acaaa24f180119fea0943c4019f31a2
Dev Jayswal <rdssjayswal@gmail.com>
2024-06-28 21:16:32 +0530

    death

--- ChatBot/app.js
+++ ChatBot/app.js
@@ -208,19 +208,19 @@ app.get("/", (req, res) => {
 
 
 
-const httpsServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname,'cert' ,'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem')),
-},
-app);
+// const httpsServer = https.createServer({
+//   key: fs.readFileSync(path.join(__dirname,'cert' ,'key.pem')),
+//   cert: fs.readFileSync(path.join(__dirname, 'cert','cert.pem')),
+// },
+// app);
 
+// httpsServer.listen(port, () => {
+//   console.log(`Server is running on https://localhost:${port}`);
+// });
 
-
-
-httpsServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on https://localhost:${port}`);
 });
-
 
 /*
 import MistralClient from '@mistralai/mistralai';
